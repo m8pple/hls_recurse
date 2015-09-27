93b42158024a92b5f945da57c87fa9a353914815
m8pple <dt10@imperial.ac.uk>
2015-09-28 00:10:10 +0100

    Missing wrapper.

---
+++ include/hls_recurse/examples/sudoku_man.hpp
@@ -0,0 +1,32 @@
+#ifndef sudoku_man_hpp
+#define sudoku_man_hpp
+
+#include "hls_recurse/state_machine_self_recursion.hpp"
+#include <stdint.h>
+
+#include "hls_recurse/examples/sudoku.hpp"
+
+namespace hls_recurse
+{
+
+bool f2_sudoku_man(int *puzzle)
+{
+    return man_sudoku(puzzle);
+}
+
+
+template<class T>
+bool test_sudoku_man(T sudoku, bool logEvents=false)
+{
+    return test_sudoku(sudoku, logEvents);
+}
+
+template<class T>
+int harness_sudoku_man(T sudoku, bool logEvents=false)
+{
+    return harness_sudoku(sudoku, logEvents);
+}
+
+}; // hls_recurse
+
+#endif
